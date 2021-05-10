d2bd7224bc290f5505727e14512aa6e222e1c6ac
Mehmet Tercueman <mehmet.duhan.tercueman@mni.thm.de>
2021-05-10 18:55:46 +0200

    Alles in eine NJVM Datei packen

--- njvm.c
+++ njvm.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#define VERSION 3
+#define VERSION 2
 #define MAXSIZE 10000
 
 int sp = 0; //Stackpointer
@@ -11,14 +11,50 @@ int pc = 0; //Programmcounter
 
 unsigned int *program_memory;
 int stack[MAXSIZE];
-unsigned int counter;
+unsigned int ProgramCounter;
 bool halt = false;
 char *file_Path;
 FILE *filePointer;
 char charNJBF[4];
 unsigned int *SDA;
 unsigned int FramePointer = 0;
-bool lowerUpper;
+
+unsigned int program_1[] = {
+
+    (PUSHC << 24) | IMMEDIATE(3),
+    (PUSHC << 24) | IMMEDIATE(4),
+    (ADD << 24),
+    (PUSHC << 24) | IMMEDIATE(10),
+    (PUSHC << 24) | IMMEDIATE(6),
+    (SUB << 24),
+    (MUL << 24),
+    (WRINT << 24),
+    (PUSHC << 24) | IMMEDIATE(10),
+    (WRCHR << 24),
+    (HALT << 24)};
+
+unsigned int program_2[] = {
+
+    (PUSHC << 24) | IMMEDIATE(-2),
+    (RDINT << 24),
+    (MUL << 24),
+    (PUSHC << 24) | IMMEDIATE(3),
+    (ADD << 24),
+    (WRINT << 24),
+    (PUSHC << 24) | IMMEDIATE('\n'),
+    (WRCHR << 24),
+    (HALT << 24)
+
+};
+
+unsigned int program_3[] = {
+    (RDCHR << 24),
+    (WRINT << 24),
+    (PUSHC << 24) | IMMEDIATE('\n'),
+    (WRCHR << 24),
+    (HALT << 24)
+
+};
 
 // Push
 void push(int i)
@@ -34,19 +70,12 @@ int pop(void)
     return stack[sp];
 }
 
-void rdchr(void)
-{
-    char val;
-    printf("Insert a Character please!\n");
-    scanf("%c", &val);
-    push((int)val);
-}
-
 void execute(unsigned int ins)
 {
     int opCode = ins >> 24;
     int immediate = SIGN_EXTEND(IMMEDIATE(ins));
     int val1, val2, val3, tmp;
+    char cval;
 
     switch (opCode)
     {
@@ -121,8 +150,9 @@ void execute(unsigned int ins)
         break;
 
     case RDCHR:
-
-        rdchr();
+        printf("Insert a Character please!\n");
+        scanf("%c", &cval);
+        push((int)cval);
         break;
 
     case WRCHR:
@@ -158,98 +188,7 @@ void execute(unsigned int ins)
         tmp = pop();
         stack[FramePointer + immediate] = tmp;
         break;
-    case EQ:
-        tmp = pop();
-        val2 = pop();
-        if (tmp == val2)
-        {
-            push(true);
-        }
-        else
-        {
-            push(false);
-        }
-        break;
-    case NE:
-        tmp = pop();
-        val2 = pop();
-        if (tmp != val2)
-        {
-            push(true);
-        }
-        else
-        {
-            push(false);
-            
-        }
-        break;
-    case LT:
-        tmp = pop();
-        val2 = pop();
-        if (val2 < tmp)
-        {
-            push(true);
-            
-        }
-        else
-        {
-            push(true);
-    
-        }
-        break;
-    case LE:
-        tmp = pop();
-        val2 = pop();
-        if (val2 <= tmp)
-        {
-            push(true);
-        }
-        else
-        {
-            push(false);
-        }
-        break;
-    case GT:
-        tmp = pop();
-        val2 = pop();
-        if (val2 > tmp)
-        {
-            push(true);
-        }
-        else
-        {
-            push(false);
-        }
-        break;
-    case GE:
-        tmp = pop();
-        val2 = pop();
-        if (val2 >= tmp)
-        {
-            push(true);
-            
-        }
-        else
-        {
-            push(false);
-            
-        }
-        break;
-    case JMP:
-        counter = immediate;
-        break;
-    case BRF:
-        if (pop() == false)
-        {
-            counter = immediate;
-        }
-        break;
-    case BRT:
-        if (pop() == true)
-        {
-            counter = immediate;
-        }
-        break;
+
     default:
         printf("Unbekannte Eingabe!\n");
         break;
@@ -276,88 +215,90 @@ int main(int argc, char *argv[])
         {
             printf("Ninja Virtual Machine version 0 (compiled April 18 2021, 16:34:52)\n");
         }
-        else if (strcmp(argv[i], "--debug") == 0)
+        else if (strcmp(argv[i], "1") == 0)
         {
-            int a;
-            printf("Debugger is started. Select an operation:\n");
-            printf("[0] Show the Stack [1] Show the SDA [2] Only the next Instruction [3] Execute all Instructions [4] Exit\n");
-            scanf("%d", a);
-            switch (a)
-            {
-            case 0:
-                break;
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            }
+            program_memory = program_1;
+            printf("Test 1 \n");
+        }
+        else if (strcmp(argv[i], "2") == 0)
+        {
+            program_memory = program_2;
+            printf("Test 2 \n");
+        }
+        else if (strcmp(argv[i], "3") == 0)
+        {
+            program_memory = program_3;
+            printf("Test 3 \n");
+        }
+        else if (strcmp(argv[i], "FILE") == 0)
+        {
+            program_memory = program_3;
+            printf("Test 3 \n");
         }
         else
         {
             printf("unknown command line argument '%s', try './njvm --help'\n", argv[i]);
         }
     }
+    
+    
+        file_Path = argv[argc - 1];
+        if ((filePointer = fopen(file_Path, "r")) == NULL)
+        {
+            perror("The File is not there!");
+            exit(1);
+        }
 
-    file_Path = argv[argc - 1];
-    if ((filePointer = fopen(file_Path, "r")) == NULL)
-    {
-        perror("The File is not there!");
-        exit(1);
-    }
+        fread(charNJBF, sizeof(char), 4, filePointer);
 
-    fread(charNJBF, sizeof(char), 4, filePointer);
+        if (strncmp(charNJBF, "NJBF", 4) != 0)
+        {
+            perror("Wrong Directory! \n");
+            exit(1);
+        }
+        unsigned int version;
 
-    if (strncmp(charNJBF, "NJBF", 4) != 0)
-    {
-        perror("Wrong Directory! \n");
-        exit(1);
-    }
-    unsigned int version;
+        fread(&version, sizeof(unsigned int), 1, filePointer);
 
-    fread(&version, sizeof(unsigned int), 1, filePointer);
+        if (version > VERSION)
+        {
+            perror("Wrong Version\n");
+            exit(1);
+        }
 
-    if (version > VERSION)
-    {
-        perror("Wrong Version\n");
-        exit(1);
-    }
+        unsigned int instructionsSize;
 
-    unsigned int instructionsSize;
+        fread(&instructionsSize, sizeof(unsigned int), 1, filePointer);
 
-    fread(&instructionsSize, sizeof(unsigned int), 1, filePointer);
+        program_memory = malloc(instructionsSize * sizeof(unsigned int));
 
-    program_memory = malloc(instructionsSize * sizeof(unsigned int));
+        if (program_memory == NULL)
+        {
+            perror("program memory is empty\n");
+            exit(1);
+        }
 
-    if (program_memory == NULL)
-    {
-        perror("program memory is empty\n");
-        exit(1);
-    }
+        unsigned int SDASize;
 
-    unsigned int SDASize;
+        fread(&SDASize, sizeof(unsigned int), 1, filePointer);
 
-    fread(&SDASize, sizeof(unsigned int), 1, filePointer);
+        SDA = malloc(SDASize * sizeof(unsigned int));
 
-    SDA = malloc(SDASize * sizeof(unsigned int));
+        if (SDA == NULL)
+        {
+            perror("SDA is Empty!\n");
+            exit(1);
+        }
 
-    if (SDA == NULL)
-    {
-        perror("SDA is Empty!\n");
-        exit(1);
-    }
-
-    fread(program_memory, sizeof(unsigned int), instructionsSize, filePointer);
+        fread(program_memory, sizeof(unsigned int), instructionsSize, filePointer);
+    
 
     printf("Ninja Virtual Machine started\n");
     while (!halt)
     {
-        counter = program_memory[pc];
+        ProgramCounter = program_memory[pc];
         pc++;
-        execute(counter);
+        execute(ProgramCounter);
     }
     printf("Ninja Virtual Machine stopped\n");
     return 0;
